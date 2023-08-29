25b023a785f6416433248c8b875495718d139246
HenryHK <henryhan.hku@gmail.com>
2023-08-29 15:43:07 +1000

    update colorschema

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -24,7 +24,6 @@ return {
   -- Disable ligatures
   -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = {"zero"},
-  
 
   -- Cursor style
   default_cursor_style = 'BlinkingBar',
@@ -33,15 +32,16 @@ return {
   -- https://wezfurlong.org/wezterm/config/lua/config/enable_csi_u_key_encoding.html
   enable_csi_u_key_encoding = true,
   -- custom colors - tokyonight https://github.com/folke/tokyonight.nvim/blob/main/extras/wezterm_tokyonight_storm.toml 
-  colors={
-    foreground = "#c0caf5",
-    background = "#24283b",
-    cursor_bg = "#c0caf5",
-    cursor_border = "#c0caf5",
-    cursor_fg = "#24283b",
-    selection_bg = "#364A82",
-    selection_fg = "#c0caf5",
-    ansi = {"#1D202F", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6"},
-    brights = {"#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5"},
-  }
+  -- colors={
+  --   foreground = "#c0caf5",
+  --   background = "#24283b",
+  --   cursor_bg = "#c0caf5",
+  --   cursor_border = "#c0caf5",
+  --   cursor_fg = "#24283b",
+  --   selection_bg = "#364A82",
+  --   selection_fg = "#c0caf5",
+  --   ansi = {"#1D202F", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6"},
+  --   brights = {"#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5"},
+  -- },
+  color_scheme = 'Gruvbox dark, soft (base16)'
 }
